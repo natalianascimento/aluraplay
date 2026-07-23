d1de0ff631017035c3f1e68c5f27adbd74a01d26
Natalia <natalia.nascc@gmail.com>
2026-07-22 22:55:34 -0300

    Reduzindo a duplicidade de html

--- listagem-videos.php
+++ listagem-videos.php
@@ -12,36 +12,8 @@ if (isset ($_GET['sucesso'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php require_once 'inicio-html.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/estilos.css">
-    <link rel="stylesheet" href="./css/flexbox.css">
-    <title>AluraPlay</title>
-    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
-</head>
-
-<body>
-
-    <header>
-
-        <nav class="cabecalho">
-            <a class="logo" href="./"></a>
-
-            <div class="cabecalho__icones">
-                <a href="./novo-video" class="cabecalho__videos"></a>
-                <a href="./pages/login.html" class="cabecalho__sair">Sair</a>
-            </div>
-        </nav>
-
-    </header>
-    
     <?php if (isset($mensagem)): ?>
         <p class="mensagem__status"> <?= $mensagem ?> </p>
     <?php endif; ?>
@@ -67,7 +39,4 @@ if (isset ($_GET['sucesso'])) {
         <?php endforeach; ?>
     </ul>
 
-   
-</body>
-
-</html>
+<?php require_once 'fim-html.php'; ?>
