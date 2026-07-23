d453b9aa7a9f75c8fa92fc39a5001a1a54f56a98
Natalia <natalia.nascc@gmail.com>
2026-07-22 22:42:19 -0300

    Reestruturação do código

--- editar-video.php
+++ editar-video.php
@@ -1,23 +1,32 @@
 <?php
+
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 
-$id = filter_input(INPUT_POST, 'id');
-$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
-$titulo = filter_input(INPUT_POST, 'titulo');
-
-if($id === false) {
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+if ($id === false) {
     header('Location: /index.php?sucesso=0');
     exit();
-} 
+}
 
-$sql = "UPDATE videos SET url = :url, title = :titulo WHERE ID = :id;";
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+    header('Location: /index.php?sucesso=0');
+    exit();
+}
+$titulo = filter_input(INPUT_POST, 'titulo');
+if ($titulo === false) {
+    header('Location: /index.php?sucesso=0');
+    exit();
+}
+
+$sql = 'UPDATE videos SET url = :url, title = :title WHERE id = :id;';
 $statement = $pdo->prepare($sql);
 $statement->bindValue(':url', $url);
-$statement->bindValue(':titulo', $titulo);
+$statement->bindValue(':title', $titulo);
 $statement->bindValue(':id', $id, PDO::PARAM_INT);
 
-if($statement->execute() === false) {
+if ($statement->execute() === false) {
     header('Location: /index.php?sucesso=0');
 } else {
     header('Location: /index.php?sucesso=1');
