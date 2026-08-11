9657ddf11ac9931b3e9eb507d8f450492c75f4e4
Natalia <natalia.nascc@gmail.com>
2026-08-11 15:15:04 -0300

    Logout

--- config/routes.php
+++ config/routes.php
@@ -8,5 +8,6 @@ return [
     'POST|/editar-video' => ['class' => \Alura\Mvc\Controller\EditaVideoController::class, 'repository' => \Alura\Mvc\Repository\VideoRepository::class],
     'GET|/remover-video' => ['class' => \Alura\Mvc\Controller\RemoveVideoController::class, 'repository' => \Alura\Mvc\Repository\VideoRepository::class],
     'GET|/login' => ['class' => \Alura\Mvc\Controller\LoginFormController::class, 'repository' => \Alura\Mvc\Repository\UserRepository::class],
-    'POST|/login' => ['class' => \Alura\Mvc\Controller\LoginController::class, 'repository' => \Alura\Mvc\Repository\UserRepository::class]
+    'POST|/login' => ['class' => \Alura\Mvc\Controller\LoginController::class, 'repository' => \Alura\Mvc\Repository\UserRepository::class],
+    'GET|/logout' => ['class' => \Alura\Mvc\Controller\LogoutController::class, 'repository' => \Alura\Mvc\Repository\UserRepository::class]
 ];
---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alura\Mvc\Controller;
+
+class LogoutController implements Controller
+{
+    public function processaRequisicao(): void
+    {
+        unset($_SESSION['logado']);
+        header('location: /login');
+    }
+
+}
